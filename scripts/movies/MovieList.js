67bad80833ec5d5fcb7ba2867c5aa148db4b561e
John Douglas Price <johndouglasprice@Johns-MacBook-Pro.local>
2021-01-19 10:51:38 -0500

    Added basic styling

--- scripts/movies/MovieList.js
+++ scripts/movies/MovieList.js
@@ -8,10 +8,8 @@ export function MovieCard() {
 
     let movieListHTMLString= ""
     for(const currentMovieInLoop of allTheMovies) {
-        console.log(currentMovieInLoop)
         movieListHTMLString += Movie(currentMovieInLoop)
     }
 
-    console.log(movieListHTMLString)
-    movieContainer.innerHTML = `<h1 class="movie-list-heading">List of Movies</h1>${movieListHTMLString}`
+    movieContainer.innerHTML = `${movieListHTMLString}`
 }
